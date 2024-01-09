2c7c450a376d6acc85046f0302a0df923533d407
Ahmad Raza <ahmadelectricaltraders@gmail.com>
2024-01-09 22:24:07 +0500

    09-01-2023 30 min

--- temp.py
+++ temp.py
@@ -6,49 +6,48 @@ import json
 
 app = Flask(__name__)
 
-
 def file_to_base64(file):
     try:
         return base64.b64encode(file.read()).decode('utf-8')
     except Exception as e:
         print(f"Error encoding file to base64: {str(e)}")
         return None
+    
 
-
-def create_template(base64_data, subject_of_email):
-    url = "https://demo.docusign.net/restapi//v2.1/accounts/bd182fdb-bda4-40d6-85b5-0a452eaef7e6/templates"
+def create_envelope(base64_data, name, email, subject_of_email):
+    url = "https://demo.docusign.net/restapi//v2.1/accounts/bd182fdb-bda4-40d6-85b5-0a452eaef7e6/envelopes"
 
     file_path = 'file.json'
     with open(file_path, 'r') as file:
         data = json.load(file)
 
     payload = json.dumps({
-    "documents": [
-        {
-        "name": "agreement",
-        "documentBase64": f"{base64_data}",
-        "documentId": "1234",
-        "fileExtension": "docx"
-        }
-    ],
-    "emailBlurb": "Email message",
-    "emailSubject": f"{subject_of_email}",
-    "recipients": {
-        "signers": [
-        {
-            "recipientId": "1",
-            "roleName": "seller"
-        }
-        ]
-    }
+        "documents": [
+            {
+                "documentBase64": f"{base64_data}",
+                "documentId": 1234,
+                "fileExtension": "docx",
+                "name": "Malik Ahmad"
+            }
+        ],
+        "emailSubject": f"{subject_of_email}",
+        "recipients": {
+            "signers": [
+                {
+                    "email": f"{email}",
+                    "name": f"{name}",
+                    "recipientId": "451"
+                }
+            ]
+        },
+        "status": "sent"
     })
     headers = {
-    'Content-Type': 'application/json',
-    'Authorization': f"Bearer {data['access_token']}"
+        'Content-Type': 'application/json',
+        'Authorization': f"Bearer {data['access_token']}"
     }
 
     response = requests.request("POST", url, headers=headers, data=payload)
-
     return response.text
 
 
@@ -98,15 +97,12 @@ def access_token():
                 json.dump(res, json_file)
 
 
-@app.route('/template')
-def template():
-    return render_template('template.html')
-
-
-@app.route('/templateUpload', methods=['POST'])
-def templateUpload():
+@app.route('/upload', methods=['POST'])
+def upload_file():
     if request.method == 'POST':
 
+        name = request.form['name']
+        email = request.form['email']
         subject_of_email = request.form['subject_of_email']
 
         if 'file' not in request.files:
@@ -122,63 +118,45 @@ def templateUpload():
 
         # access Key docusign
         access_token()
-
-        template = create_template(base64_data, subject_of_email) 
-        file_path = 'template.json'
-        with open(file_path, 'r') as file:
-            data = json.load(file)
-        data.append(template)
         
-        file_path = 'template.json'
-        with open(file_path, 'w') as json_file:
-            json.dump(data, json_file)
-        
-        return "success"
-        # return envelope
-        
-
-@app.route("/send_envelope", methods=['GET','POST'])
-def send_envelope():
-    if request.method == 'POST':
-        name = request.form['username']
-        email = request.form['email']
-        template_id = request.form['template_id']
-
-        url = "https://demo.docusign.net/restapi//v2.1/accounts/bd182fdb-bda4-40d6-85b5-0a452eaef7e6/envelopes"
-
-        file_path = 'file.json'
-        with open(file_path, 'r') as file:
-            data = json.load(file)
-
-        payload = json.dumps({
-        "templateId": f"{template_id}",
-        "templateRoles": [
-            {
-            "email": f"{email}",
-            "name": f"{name}",
-            "roleName": "Signer"
-            }
-        ],
-        "status": "sent"
-        })
-        headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f"Bearer {data['access_token']}"
-        }
-
-        response = requests.request("POST", url, headers=headers, data=payload)
-
-        return response.text
+        try:
+            if name and email and subject_of_email and  base64_data:
+                # create envelope
+                envelope = create_envelope(base64_data, name, email, subject_of_email)
+                # new working
+                file_path = 'envelope.json'
+                with open(file_path, 'r') as file:
+                    data = json.load(file)
+                data.append(envelope)
+                print(data)
+                file_path = 'envelope.json'
+                with open(file_path, 'w') as json_file:
+                    json.dump(data, json_file)
+                return envelope
             
+        except:
+            return "try again"
         
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/template_view')
+def template_view():
     file_path = 'template.json'
     with open(file_path, 'r') as file:
         data = json.load(file)
 
     template_list = [json.loads(item) for item in data]
-    return render_template("send_envelope.html", template_list = template_list)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    print(template_list)
+    return render_template("template_view.html", template_list = template_list)
 
 
+@app.route('/envelope_view')
+def envelope_view():
+    file_path = 'envelope.json'
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    envelope_list = [json.loads(item) for item in data]
+    return render_template("envelope_view.html", envelope_list = envelope_list)
